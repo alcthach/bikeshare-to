9753c37f8652b6f238c630f4f4d768f5a754ba97
athach <alcthach@gmail.com>
2022-09-26 14:26:03 -0400

    WIP: Still working on cleaning up datetime column, all that's left is string replacement for year and then some concatenation into appropriate datetime format

--- src/2017-2018_preprocessing.sql
+++ src/2017-2018_preprocessing.sql
@@ -111,3 +111,77 @@ FROM
 -- Another important thing to note is that when I order `trip_id` by ASC, it's doing so as a string rather than a numeric value
 -- I'm running into some issues here because this isn't actually in the correct order
 -- Question to figure out is what is the pattern that postgres uses when ordering a column that has numeric characters? 
+
+-- 20220926
+
+-- I might be able to employ a case when statement to ensure that I map the start_time_substrings to the correct day or month COLUMNS 
+
+-- Employing a pattern like case when trip id is greater 1253914, substring1 = day, substring2 = MONTH 
+-- else substring1 = month, substring2 = day
+-- It's a very manual way of doing it; Re: I had figured out the trip_id where the assumed change in date format had happened
+-- Although, this is only an assumption, however when I go on to perform my transforms, I expect that I won't have an out of range error
+
+-- Actually, since the the substrings live in their own columns as type integer
+-- I might be able to just go through each row, see if... actually never mind that would be a disaster
+-- I could assume that there wouldn't be such a random switch in date formats, and I could expect to see a phase shift in date format as seen in the notebook
+
+CREATE TABLE trips_2017_2018_transformed AS
+	SELECT trip_id,
+			trip_start_time,
+			start_time_substring1,
+			start_time_substring2,
+			regexp_replace(trip_start_time, ' ', '/') AS regex_replaced_trip_start_time,
+			CASE
+				WHEN trip_id < 1253914 THEN start_time_substring1
+				ELSE start_time_substring2
+			END start_time_day,
+			CASE 
+				WHEN trip_id < 1253914 THEN start_time_substring2
+				ELSE start_time_substring1
+			END start_time_month,
+			split_part(trip_start_time, ' ', 2) AS start_time_mmss
+	FROM start_time_exploded ste 
+	ORDER BY trip_id;
+
+ALTER TABLE trips_2017_2018_transformed 
+	ADD trip_start_time_year text; 
+
+UPDATE trips_2017_2018_transformed 
+	SET trip_start_time_year = split_part(regex_replaced_trip_start_time, '/', 3);
+
+-- Sanity check!
+SELECT *
+FROM
+	(SELECT *
+	FROM 
+		(
+		SELECT  
+		   DISTINCT ON (start_time_day, start_time_month) trip_id, start_time_day, start_time_month, trip_start_time
+		FROM trips_2017_2018_transformed tt 
+		) AS t5
+	ORDER BY trip_id ASC) AS t6;
+	
+-- Another sanity check to ensure I'm not missing any data
+SELECT COUNT(*) AS total_rows_transformed_table
+FROM trips_2017_2018_transformed tt;
+
+SELECT COUNT(*) AS total_rows_temp_table
+FROM temp_table tt;
+
+-- Looks good :)
+
+SELECT *
+FROM trips_2017_2018_transformed tt 
+LIMIT 20;
+
+SELECT regexp_replace(trip_start_time, ' ', '/') AS YEAR
+FROM trips_2017_2018_transformed tt 
+LIMIT 5;
+
+-- Just a quick sanity check; TODO: replace '17' with '2017' and then reduce to datetime format as text, then resume the transformations from a while back...
+SELECT trip_start_time_year
+FROM trips_2017_2018_transformed tt 
+GROUP BY trip_start_time_year;
+
+SELECT * 
+FROM trips_2017_2018_transformed tt;
