dc66c7a3ae7b52ed0bf0be338202f3744a941b20
Alex Thach <alcthach@gmail.com>
2022-12-19 19:53:28 -0500

    revise to include more tables Re: further segmentation of data

--- src/init-tables/init-raw-tables.sql
+++ src/init-tables/init-raw-tables.sql
@@ -1,50 +1,108 @@
 
-CREATE TABLE IF NOT EXISTS raw_2017_Q1Q2 (
-	trip_id			varchar,
-	trip_start_time 	varchar,
-	trip_stop_time 		varchar,
-	trip_duration_seconds 	varchar,
-	from_station_id 	varchar,
-	from_station_name	varchar,
-	to_station_id		varchar,
-	to_station_name		varchar,
-	user_type		varchar
+-- ASCII
+CREATE TABLE IF NOT EXISTS raw_2017_q1q2 (
+	trip_id			text,
+	trip_start_time 	text,
+	trip_stop_time 		text,
+	trip_duration_seconds 	text,
+	from_station_id 	text,
+	from_station_name	text,
+	to_station_id		text,
+	to_station_name		text,
+	user_type		text
 );
 
-
-CREATE TABLE IF NOT EXISTS raw_2017_Q3Q4 (
-	trip_id			varchar,
-	trip_start_time 	varchar,
-	trip_stop_time 		varchar,
-	trip_duration_seconds 	varchar,
-	from_station_name	varchar,
-	to_station_name		varchar,
-	user_type		varchar
+-- ASCII
+CREATE TABLE IF NOT EXISTS raw_2017_q3q4 (
+	trip_id			text,
+	trip_start_time 	text,
+	trip_stop_time 		text,
+	trip_duration_seconds 	text,
+	from_station_name	text,
+	to_station_name		text,
+	user_type		text
 );
 
-
+-- UTF-8
 CREATE TABLE IF NOT EXISTS raw_2018 (
-	trip_id			varchar,
-	trip_duration_seconds	varchar,
-	from_station_id		varchar,
-	trip_start_time		varchar,
-	from_station_name	varchar,
-	trip_stop_time		varchar,
-	to_station_id		varchar,
-	to_station_name		varchar,
-	user_type		varchar
+	trip_id			text,
+	trip_duration_seconds	text,
+	from_station_id		text,
+	trip_start_time		text,
+	from_station_name	text,
+	trip_stop_time		text,
+	to_station_id		text,
+	to_station_name		text,
+	user_type		text
+);
+
+-- UTF-8	
+CREATE TABLE IF NOT EXISTS raw_2019_2020 (
+	"Trip Id"		text,
+	"Trip  Duration"	text,
+	"Start Station Id"	text,
+	"Start Time" 		text,
+	"Start Station Name"	text,
+	"End Station Id"	text,
+	"End Time"		text,
+	"End Station Name"	text,
+	"Bike Id"		text,
+	"User Type"		text
+);
+
+-- WINDOWS-1258
+CREATE TABLE IF NOT EXISTS raw_jan_2021 (
+	"Trip Id"		text,
+	"Trip  Duration"	text,
+	"Start Station Id"	text,
+	"Start Time" 		text,
+	"Start Station Name"	text,
+	"End Station Id"	text,
+	"End Time"		text,
+	"End Station Name"	text,
+	"Bike Id"		text,
+	"User Type"		text
+);
+
+-- UTF-8
+CREATE TABLE IF NOT EXISTS raw_feb_apr_2021 (
+	"Trip Id"		text,
+	"Trip  Duration"	text,
+	"Start Station Id"	text,
+	"Start Time" 		text,
+	"Start Station Name"	text,
+	"End Station Id"	text,
+	"End Time"		text,
+	"End Station Name"	text,
+	"Bike Id"		text,
+	"User Type"		text
+);
+
+-- EUC-TW
+CREATE TABLE IF NOT EXISTS raw_may_2021 (
+	"Trip Id"		text,
+	"Trip  Duration"	text,
+	"Start Station Id"	text,
+	"Start Time" 		text,
+	"Start Station Name"	text,
+	"End Station Id"	text,
+	"End Time"		text,
+	"End Station Name"	text,
+	"Bike Id"		text,
+	"User Type"		text
 );
 
 
-CREATE TABLE IF NOT EXISTS raw_2019_present (
-	"Trip Id"		varchar,
-	"Trip  Duration"	varchar,
-	"Start Station Id"	varchar,
-	"Start Time" 		varchar,
-	"Start Station Name"	varchar,
-	"End Station Id"	varchar,
-	"End Time"		varchar,
-	"End Station Name"	varchar,
-	"Bike Id"		varchar,
-	"User Type"		varchar
+-- UTF-8 
+CREATE TABLE IF NOT EXISTS raw_jun_2021_present (
+	"Trip Id"		text,
+	"Trip  Duration"	text,
+	"Start Station Id"	text,
+	"Start Time" 		text,
+	"Start Station Name"	text,
+	"End Station Id"	text,
+	"End Time"		text,
+	"End Station Name"	text,
+	"Bike Id"		text,
+	"User Type"		text
 );
