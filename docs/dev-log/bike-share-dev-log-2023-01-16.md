b84d96dea34e19c63ce94f0f50bdc03ba6f9668d
athach <alcthach@gmail.com>
2023-01-16 18:04:51 -0500

    continue data cleaning as per checklist found in script

---
+++ docs/dev-log/bike-share-dev-log-2023-01-16.md
@@ -0,0 +1,8 @@
+[[2023-01-16]]
+
+---
+16:10
+
+A lot of the cleaning has been completed. However, I'm now faced with having to impute the missing station IDs. The tricky part is this data lives in JSON format. Which luckily I'm familiar with from working with the FPL project. 
+
+I'll need to do a bit of profiling of this data. I could make my own reference table to save on time instead. But I think I'll see if I could pull the data from the endpoint.
--- src/bike-share-toronto-trips-cleaning-2023-01-10.sql
+++ src/bike-share-toronto-trips-cleaning-2023-01-10.sql
@@ -33,7 +33,8 @@ TODO
 - [-] Drop duplicate ￼￼trip_id￼￼ rows
 - [-] Investigate issue with ￼￼year￼￼ value in the ￼￼timestamp￼￼ fields; Re: Change ￼￼0017￼￼ to ￼￼2017￼￼
 - [-] Transform '0018' values
-- [ ] Drop rows with null trip duration, this accounts for about 0.07% of the data, doesn't seem to useful in gleaning insights of bike ridership
+- [-] Drop rows with null trip duration, this accounts for about 0.07% of the data, doesn't seem to useful in gleaning insights of bike ridership
+- [-] Drop rows w/ zero-value trip duration accounts for 0.06% of all data
 - [ ] Impute correct station id values in 2017 data
 - [ ] Import correct station name for
 - [ ]- Impute missing start and end station values in the data set
@@ -177,6 +178,74 @@ from trips_clean
 group by year;
 
 select split_part(trip_end_time::text, '-', 1) as year,
-       count(*)
+       count(*):
 from trips_clean
 group by year;
+
+-- Dropping rows with null trip duration values
+select *
+from trips_clean
+where trip_duration_seconds is null;
+    -- 16 rows out of 16 million
+    -- Overall, these records don't appear to be useful
+
+delete from trips_clean
+where trip_duration_seconds is null;
+
+-- Also checking for zero-value records
+select *
+from trips_clean
+where trip_duration_seconds = 0;
+
+select count(*)
+from trips_clean
+where trip_duration_seconds = 0;
+    -- About 11k rows, I don't think I've profiled this data yet...
+
+-- Looking at the profiling script, doesn't look like I looked at the zero-value records
+
+-- Brief detour to take a look at this field
+
+-- Look to see if there are any same start/end trips
+select count(*)
+from trips_clean
+where trip_duration_seconds = 0 ;
+    --  10951, about 0.06% of the total data
+
+-- Actually, it might help address the null values found in the data set first
+
+-- By referencing my docs it seems like I'm missing data in the start/end station IDs
+
+select *
+from trips_clean
+where start_station_id is null;
+
+-- I don't think it actually matters though
+select count(*)
+from trips_clean
+where trip_duration_seconds = 0 and end_station_id like 'NULL';
+    -- 3336
+
+select *
+from trips_clean
+where trip_duration_seconds = 0 and end_station_id like 'NULL';
+
+-- I'll have to write an explanation about dropping these rows
+
+-- Maybe I can take a look at the days and or stations this issue might have arised from
+-- bike_id toovvv
+
+select start_station_id,
+       count(*)
+from trips_clean
+where trip_duration_seconds = 0
+group by start_station_id
+order by count desc;
+
+-- Drop zero-value trip duration records
+delete from trips_clean
+where trip_duration_seconds = 0;
+
+select *
+from trips_clean
+where start_station_id
