784a1b80bf2c42a5f346571d900d2f77c7264d76
athach <alcthach@gmail.com>
2023-03-06 14:51:50 -0500

    initial commit

---
+++ src/bst-trips-preprocessing-prod-20230306.sql
@@ -0,0 +1,9 @@
+/*
+ Bike Share Toronto - Data Preprocessing Script
+ 2023-03-06
+ alcthach@gmail.com
+
+ Purpose: This scripts contains all the necessary queries to prepare the data for future use in production
+ */
+
+  
